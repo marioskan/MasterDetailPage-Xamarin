78c430cbf7f270b53578ea63bf3a0a6aba93d3b9
marioskan <marios1997@yahoo.gr>
2020-02-09 14:53:07 +0200

    update

---
+++ Master/Master/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Master
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public List<MasterPageItem> menuList { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            menuList = new List<MasterPageItem>();
+            menuList.Add(new MasterPageItem() { Title = "First", Icon = "", TargetType = typeof(FirstPage) });
+            menuList.Add(new MasterPageItem() { Title = "Second", Icon = "", TargetType = typeof(SecondPage) });
+
+            navigationList.ItemsSource = menuList;
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(FirstPage)));
+
+            this.BindingContext = new
+            {
+                Footer = "Example"
+            };
+        }
+
+        private void NavigationList_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = (MasterPageItem)e.SelectedItem;
+            Type page = item.TargetType;
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(page));
+            IsPresented = false;
+
+        }
+    }
+}
---
+++ Master/Master/MasterPageItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Master
+{
+    public class MasterPageItem
+    {
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
